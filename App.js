d3a91e55e145521aa2e76d2779136bf67dce265b
valdeilson <valdeilson.ramos@ifpi.edu.br>
2020-06-11 00:11:20 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import {StyleSheet, Image, TouchableOpacity, View, Text} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('./src/cronometro.png')}
+          style={styles.cronometro}
+        />
+        <Text style={styles.timer}>0.0</Text>
+        <View style={styles.btnArea}>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnTexto}>VAI</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnTexto}>PARAR</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00aeef',
+  },
+  timer: {
+    marginTop: -160,
+    color: '#FFF',
+    fontSize: 65,
+    fontWeight: 'bold',
+  },
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 70,
+    height: 40,
+  },
+  btn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9,
+  },
+  btnTexto: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef',
+  },
+});
+
+export default App;
